ed09bb9ffec39bf760c69437068cbc19647de814
Sean Brandt <seanb4t@fzymgc.dev>
2021-10-23 00:01:19 -0400

    initial zgenom and starter kit

---
+++ run_once_install_zgenom.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+git clone https://github.com/jandamm/zgenom.git "${HOME}/.zgenom"
+
---
+++ run_once_install_zsh_quickstart_kit.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+set -e 
+
+git clone git@github.com:unixorn/zsh-quickstart-kit.git "${HOME}/.zsh_quickstart_kit"
+[[ -d ~/.fzf ]] || git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
+
+( cd  "${HOME}/.zsh_quickstart_kit" && stow --target="${HOME}" zsh )
+
