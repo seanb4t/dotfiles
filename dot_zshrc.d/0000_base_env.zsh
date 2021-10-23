0f42972126786b32de2b5393ff9c068ab9f7da0a
Sean Brandt <seanb4t@fzymgc.dev>
2021-10-23 19:53:37 -0400

    aliases and editor updates

--- dot_zshrc.d/0000_base_env.zsh
+++ dot_zshrc.d/0000_base_env.zsh
@@ -1,5 +1,7 @@
+#!/usr/bin/env zsh
 # -*- mode: sh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
 # code: language=shell insertSpaces=true tabSize=2
+
 
 export DONT_PRINT_SSH_KEY_LIST=1
 export QUICKSTART_KIT_REFRESH_IN_DAYS=7
--- dot_zshrc.d/1000_general_aliases.zsh
+++ dot_zshrc.d/1000_general_aliases.zsh
@@ -2,9 +2,10 @@
 # -*- mode: sh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
 # code: language=shell insertSpaces=true tabSize=2
 
-if command -v code > /dev/null 2>&1; then
-    export EDITOR="code -w"
-    export GIT_EDITOR="code -w"
-fi
+function has() {
+  command -v $1 > /dev/null 2>&1
+}
 
-export VISUAL=vim
+has bat && alias cat=bat
+has rg && alias grep=rg && alias ack=rg
+has fd && alias find=fd
