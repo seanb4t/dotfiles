00d826468883032ca2f8a9031b38afc165590e90
Sean Brandt <seanb4t@fzymgc.dev>
2021-10-23 11:09:05 -0400

    updates, remove creds

--- dot_zshrc.d/0000_base_env.zsh
+++ dot_zshrc.d/0000_base_env.zsh
@@ -1,3 +1,11 @@
+# -*- mode: sh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# code: language=shell insertSpaces=true tabSize=2
+
 export APPLE_SSH_ADD_BEHAVIOR=1
 export DONT_PRINT_SSH_KEY_LIST=1
 export QUICKSTART_KIT_REFRESH_IN_DAYS=7
+
+#
+## Update brew every day
+#
+export HOMEBREW_AUTO_UPDATE_SECS=86400
