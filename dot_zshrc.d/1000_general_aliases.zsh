d2c750e675d08abd4fea3c87acd8a0161f0f52a9
Sean Brandt <seanb4t@fzymgc.dev>
2021-10-23 20:07:25 -0400

    more aliases

--- dot_zshrc.d/1000_general_aliases.zsh
+++ dot_zshrc.d/1000_general_aliases.zsh
@@ -6,6 +6,37 @@ function has() {
   command -v $1 > /dev/null 2>&1
 }
 
-has bat && alias cat=bat
+#
+## cat/bat
+#
+has bat && alias cat=bat && alias catp='bat -pp'
+
+#
+## grep/ack/rg
+#
 has rg && alias grep=rg && alias ack=rg
+
+#
+## find
+#
 has fd && alias find=fd
+
+
+#
+## exa / ls
+#
+if has exa; then
+  alias ls='exa -F --group-directories-first'
+  alias l='exa -lbF --group-directories-first'
+  alias ll='exa -lbGF --git --group-directories-first'
+  alias llm='exa -lbGd --git --sort=modified'                            # long list, modified date sort
+  alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+  alias lg='exa -TF'
+  alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+  alias lS='exa -1'                                                              # one column, just names
+fi
+
+#
+## top/htop
+#
+has htop && alias top=htop
