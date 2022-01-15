c7c491251c461812b0111a3e4fe0b648c5954f46
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2022-01-14 21:45:01 -0500

    change quickstart kit location

--- run_once_install_zsh_quickstart_kit.sh
+++ run_once_install_zsh_quickstart_kit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-set -e 
+set -e
 
 git clone git@github.com:unixorn/zsh-quickstart-kit.git "${HOME}/.zsh_quickstart_kit"
 [[ -d ~/.fzf ]] || git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
