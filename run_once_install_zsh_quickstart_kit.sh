c17c1e7f6f8911c3c63c18afb1d756fd8b1c67d7
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2022-01-14 21:47:06 -0500

    change quickstart kit apply

--- run_once_install_zsh_quickstart_kit.sh
+++ run_once_install_zsh_quickstart_kit.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-git clone git@github.com:unixorn/zsh-quickstart-kit.git "${HOME}/.zsh_quickstart_kit"
+[[ -d "${HOME}/.zsh_quickstart_kit" ]]; then
+  cd "${HOME}/.zsh_quickstart_kit"
+  git pull
+else
+  git clone git@github.com:unixorn/zsh-quickstart-kit.git "${HOME}/.zsh_quickstart_kit"
+fi
+
 [[ -d ~/.fzf ]] || git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
 
 ( cd  "${HOME}/.zsh_quickstart_kit" && stow --target="${HOME}" zsh )
