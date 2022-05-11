dbb41306f85f474bd99f6304ecb1b90f84432e33
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2022-05-11 10:29:42 -0400

    update order of install

--- run_once_install_00_zsh_quickstart_kit.sh
+++ run_once_install_00_zsh_quickstart_kit.sh
@@ -9,10 +9,12 @@ if [[ -d "${HOME}/.zsh_quickstart_kit" ]]; then
   cd "${HOME}/.zsh_quickstart_kit"
   git pull
 else
-  git clone git@github.com:unixorn/zsh-quickstart-kit.git "${HOME}/.zsh_quickstart_kit"
+  git clone git@github.com:seanb4t/zsh-quickstart-kit.git "${HOME}/.zsh_quickstart_kit"
 fi
 
 [[ -d ~/.fzf ]] || git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
+
+rm -rf ~/.zshrc ~/.zshrc.local ~/.zshrc.pre-oh-my-zsh ~/.zshenv ~/.zshrc.d
 
 ( cd  "${HOME}/.zsh_quickstart_kit" && stow --target="${HOME}" zsh )
 
