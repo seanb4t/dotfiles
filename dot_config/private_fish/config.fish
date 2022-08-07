2d3ee34032e86edce30b3076ced52426122bd769
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2022-08-07 09:21:55 -0400

    latest updates

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,11 +1,24 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
 
+  # Third party completions
+  ## kubectl / kubernetes
   kubectl completion fish | source
+
+  ## helm completion
+  helm completion fish | source
+
+  ## 1password cli completion, unset connect related vars
+  set -u OP_CONNECT_HOST
+  set -u OP_CONNECT_TOKEN
+  op completion fish | source
+  # flux
+  flux completion fish | source
+
 end
 
-set -Ux EDITOR nvim
-set -Ux VISUAL nvim
-set -Ux PAGER less
-set -Ux LESS "-R -M -X"
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx PAGER less
+set -gx LESS "-R -M -X"
 
