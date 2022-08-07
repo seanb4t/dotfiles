c86ce7086bbccf112a3d276b20f2fb5c34f7ebd7
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2022-08-07 12:31:21 -0400

    update ls and exa

--- dot_config/private_fish/functions/ls.fish
+++ dot_config/private_fish/functions/ls.fish
@@ -1,3 +1,3 @@
-function l --wraps='exa $EXA_STANDARD_OPTIONS $EXA_L_OPTIONS' --description 'alias l exa $EXA_STANDARD_OPTIONS $EXA_L_OPTIONS'
-  exa $EXA_STANDARD_OPTIONS $EXA_L_OPTIONS $argv;
+function ls --wraps=l --wraps='exa $EXA_STANDARD_OPTIONS $EXA_LS_OPTIONS' --description 'alias ls exa $EXA_STANDARD_OPTIONS $EXA_LS_OPTIONS'
+  exa $EXA_STANDARD_OPTIONS $EXA_LS_OPTIONS $argv; 
 end
