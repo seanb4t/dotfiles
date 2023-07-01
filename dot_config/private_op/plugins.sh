b4bfdb3808841486e1eb64d63c39387606edd143
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2023-07-01 19:55:51 -0400

    vault

---
+++ dot_config/private_op/plugins.sh
@@ -0,0 +1,3 @@
+export OP_PLUGIN_ALIASES_SOURCED=1
+alias terraform="op plugin run -- terraform"
+alias vault="op plugin run -- vault"
---
+++ dot_zshrc.d/9999_op.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+
+[[ -f ~/.config/op/plugins.sh ]] && source ~/.config/op/plugins.sh
