c6a43640165b7755c54c04dac1a9e722272313df
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2025-02-22 21:25:50 -0500

    clean up kubeconfig handling

--- dot_config/fish/conf.d/k8s.fish
+++ dot_config/fish/conf.d/k8s.fish
@@ -7,6 +7,25 @@ if test -f (dirname (realpath (status --current-filename)))/kubectl_aliases
     source (dirname (realpath (status --current-filename)))/kubectl_aliases
 end
 
-# build KUBECONFIG from ~/.kube/configs/*.yml and ~/.kube/config, using ; as separator
-set -x KUBECONFIG (ls ~/.kube/configs/*.yml ~/.kube/config | string join ":")
+# If ~/.kube/config exists, set KUBECONFIG
+if test -f ~/.kube/config
+  set -x KUBECONFIG ~/.kube/config
+else
+  set -x KUBECONFIG ""
+end
 
+# If there are any *.yml files in ~/.kube/configs, add them to KUBECONFIG
+if test -d ~/.kube/configs
+  for file in ~/.kube/configs/*.yml
+      # Only proceed if the file actually exists (pattern may not expand)
+      if test -f $file
+          if test -z $KUBECONFIG
+              # If KUBECONFIG is empty, set it directly
+              set -x KUBECONFIG $file
+          else
+              # Otherwise, append with a colon
+              set -x KUBECONFIG $KUBECONFIG:$file
+          end
+      end
+  end
+end
