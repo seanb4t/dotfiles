9a372fb1161ecd12db714e78939209cbebef6a33
seanb4t <4678+seanb4t@users.noreply.github.com>
2026-02-19 16:02:55 -0500

    Skip 1Password agent proxy setup during SSH sessions

--- dot_config/fish/conf.d/1password.fish
+++ dot_config/fish/conf.d/1password.fish
@@ -1,6 +1,11 @@
 # 1password.fish
 # A fish shell script to integrate 1Password CLI with fish shell
 
+# Do not override agent settings for inbound SSH sessions.
+if set -q SSH_CONNECTION; or set -q SSH_TTY; or set -q SSH_CLIENT
+  return
+end
+
 function mac_link_1password
   set -l source_socket ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
   set -l dest_socket ~/.1password/agent.sock
@@ -27,4 +32,3 @@ case Linux
 case '*'
   # Nothing
 end
-
